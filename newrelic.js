f374bbe5a71faf05b8c0647997821a6238155bd6
Enzo Martin <enzo.r.martin@gmail.com>
2014-01-18 14:38:12 +0100

    Create new relic app

--- newrelic.js
+++ newrelic.js
@@ -8,7 +8,7 @@ exports.config = {
   /**
    * Array of application names.
    */
-  app_name : ['Steam Backbone'],
+  app_name : ['Steam Backbone Mongo'],
   /**
    * Your New Relic license key.
    */
